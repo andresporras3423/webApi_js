ece3f4a5daf5f95f15c3880509cc747fc6fda397
oscarporras02 <oscarrussi@outlook.com>
2021-01-19 20:20:27 -0500

    add query to find a user

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -3,7 +3,7 @@ const {users} = require("../models/users.js");
 
 module.exports.usersController = (app, request, usersModel) => {
   let message= 'hello world';
-  app.get('/users', async (req, response)=>{
+  app.get('/users/all', async (req, response)=>{
     try{
       list_users = await users.find({});
       response.json({'message': list_users});
@@ -24,5 +24,17 @@ module.exports.usersController = (app, request, usersModel) => {
             console.log(err);
           }
       
+    });
+
+    app.get('/users/find', async (req, response)=>{
+      try{
+          let email = req.body.email;
+          let pass = req.body.pass;
+        list_users = await users.find({"email": email, "pass": pass});
+        response.json({'message': list_users});
+      }
+      catch(err){
+        console.log(err);
+      }
     });
 };
